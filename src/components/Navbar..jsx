f3c0e20302c92f9454fea4ba3a4dda1e890665bf
tezon <tezonteam@gmail.com>
2022-09-21 17:12:52 -0700

    new

--- src/components/Navbar..jsx
+++ src/components/Navbar..jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Logo from '../images/logo.png'
+import {navigation} from '../data'
 
 const Navbar = () => {
   return (
-    <div className=''>
-      
+    <div className='container mx-auto'>
+      <div className='flex '>
+        <div>
+          <img src={Logo} alt="logo" className='w-20' />
+        </div>
+        {navigation.map((items, index) => {
+          return (
+            <div className='p-2' key={index}>
+              <ul>
+                <li>{items.name}</li>
+              </ul>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
